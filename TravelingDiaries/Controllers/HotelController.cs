4248e60f2a71baccfa1ff3ce8aece0c11db1f9bf
subhash.gurjar <subhash.gurjar@globallogic.com>
2022-08-25 03:00:21 +0530

    Add project files.

---
+++ TravelingDiaries/Controllers/HotelController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TravelingDiaries.Models;
+
+namespace TravelingDiaries.Controllers
+{
+    public class HotelController : Controller
+    {
+
+        /*
+                to retrive all the Hotels*/
+        public async Task<IActionResult> GetAllHotels()
+        {
+            IEnumerable<Hotel> hotels = new List<Hotel>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7015/Hotel/GetAllHotels"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    hotels = JsonConvert.DeserializeObject<IEnumerable<Hotel>>(apiResponse);
+                }
+
+            }
+            return View(hotels);
+
+        }
+
+
+ /*       to get the details of single Hotel*/
+
+        public async Task<IActionResult> HotelDetail(int? id)
+        {
+            var hotel = new Hotel();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7015/Hotel/GetHotelById?id="+id))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    hotel = JsonConvert.DeserializeObject<Hotel>(apiResponse);
+                }
+
+            }
+
+            ViewData["ID"] = id;
+            return View(hotel);
+
+        }
+
+
+    }
+}
---
+++ TravelingDiaries/Controllers/OrderController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TravelingDiaries.Models;
+
+namespace TravelingDiaries.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ShoppingCart _shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            _orderRepository = orderRepository;
+            _shoppingCart = shoppingCart;
+        }
+
+
+        //This is the form for details
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+
+
+                ModelState.AddModelError("", "Your cart is empty add some pies");
+            }
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete",order);
+
+            }
+
+            return View(order);
+        }
+
+
+        public IActionResult CheckoutComplete(Order order)
+        {
+
+           
+            return View(order);
+
+        }
+   
+
+       
+    }
+
+    }
